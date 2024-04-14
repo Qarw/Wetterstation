300eba62f0a53d6a1d6afac39ff0f0756e237467
AJDIN Husic <ajdinh578@mail.com>
2024-04-14 17:54:20 +0200

    Angabe
    Angabe commiten und pushen

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    console.log("in index.js");
+    var ctx = document.getElementById('chart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
+            datasets: [{
+                label: "Temperatur [°C]",
+                data: [14, 17, 17, 18, 16, 13, 14],
+                borderColor: 'rgb(75, 192, 192)',
+                backgroundColor: 'rgb(75, 192, 192)',
+                fill: false,
+                tension: 0
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            scales: {
+                yAxes: [{
+                    type: 'linear',
+                    position: 'left',
+                    ticks: {
+                        beginAtZero: true,
+                        max: 25
+                    }
+                }]
+            }
+        }
+    });
+})
